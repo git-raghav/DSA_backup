0e2675e738449b13a2610aaddf6b3a0e6d42bf7b
raghavagarwal <raghavagarwal3618@gmail.com>
2025-07-23 02:10:12 +0530

    stacks

--- 17_Stacks/_10_duplicateParenthesis.java
+++ 17_Stacks/_10_duplicateParenthesis.java
@@ -1,24 +1,31 @@
 import java.util.Stack;
-
 public class _10_duplicateParenthesis {
-    public static boolean isValid(String str){
+    public static boolean duplicate(String str) {
         Stack<Character> stack = new Stack<>();
-        for(int i = 0; i<str.length(); i++){
+        for (int i = 0; i < str.length(); i++) {
             char ch = str.charAt(i);
-
-            //if current element is a opening bracket
-            if(ch == '(') stack.push(')');
-            else if(ch == '{') stack.push('}');
-            else if(ch == '[') stack.push(']');
-
-            //if there were no openeing bracket so stack is empty or current element is closing bracket
-            else if(stack.isEmpty() || ch != stack.pop()) return false;
+            //(, a ,b, ... sab ko push krte raho and jab ) aye toh kuch kaam karo
+            if (ch == ')') {
+                //agr ch m ) and stack k top p ( h toh iska matlab duplicasy h
+                if (stack.peek() == '(') {
+                    return true;
+                } else {
+                    //agr or koi elements h toh pop krte raho jab tak ( naa aajaye or usko bhi pop krdo
+                    while (stack.peek() != '(') { //pop everything till (
+                        stack.pop();
+                    }
+                    stack.pop();//extra pop to ( ko pop krne k liye
+                }
+            } else {
+                stack.push(ch);
+            }
         }
-        return stack.isEmpty();
+        return false;
     }
 
     public static void main(String[] args) {
-        String str = "({})[";
-        System.out.println(isValid(str));
+        // String str = "((a+b))";
+        String str = "(a+b)";
+        System.out.println(duplicate(str));
     }
 }
---
+++ 17_Stacks/_11_maxAreaHistogram.java
@@ -0,0 +1,45 @@
+import java.util.Stack;
+public class _11_maxAreaHistogram {
+    public static int calc(int arr[]) {
+        // initialization
+        int maxarea = 0;
+        int nsr[] = new int[arr.length];
+        int nsl[] = new int[arr.length];
+
+        // next smaller right. trick is to write code for next greater right then modify it.
+        Stack<Integer> stack = new Stack<>();
+        for (int i = arr.length - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && arr[stack.peek()] >= arr[i]) {
+                stack.pop();
+            }
+            if (stack.isEmpty()) nsr[i] = arr.length; //agar nsl nahi h that means area last bar tak extend ho skta h isliye length use kari ki length-1 vala include hojaye
+            else nsr[i] = stack.peek(); //we have to store index in the array and not the element itself
+            stack.push(i);
+        }
+
+        // next smaller left
+        stack = new Stack<>(); //empty the stack
+        for (int i = 0; i < arr.length; i++) {
+            while (!stack.isEmpty() && arr[stack.peek()] >= arr[i]) {
+                stack.pop();
+            }
+            if (stack.isEmpty()) nsl[i] = -1;
+            else nsl[i] = stack.peek(); //we have to store index in the array and not the element itself
+            stack.push(i);
+        }
+
+        // area calculate height * j-i-1 = height * nsr[i]-nsl[i]-1
+        for(int i = 0; i < arr.length; i++){
+            int height = arr[i];
+            int width = nsr[i] - nsl[i] - 1;
+            int currarea = height * width;
+            maxarea = Math.max(maxarea, currarea);
+        }
+        return maxarea;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 2, 4 };
+        System.out.println(calc(arr));
+    }
+}
