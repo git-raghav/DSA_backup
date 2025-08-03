fcbc0bd7df94bebc8dca0f3bd0fe16211be142e6
raghavagarwal <raghavagarwal3618@gmail.com>
2025-08-04 01:43:18 +0530

    binary trees

--- 20_Binary Tree/_12_subtreeOfAnotherTree.java
+++ 20_Binary Tree/_12_subtreeOfAnotherTree.java
@@ -11,17 +11,33 @@ public class _12_subtreeOfAnotherTree {
         }
     }
 
-    public static boolean check(Node p, Node q) {
+    public static boolean isSubtree(Node root, Node subroot) { //preorder traversal
+        if(root == null) return false;
+        if(root.data == subroot.data){
+            if(isIdentical(root, subroot)) return true;
+        }
+        return isSubtree(root.left, subroot) || isSubtree(root.right, subroot);
+    }
 
+    public static boolean isIdentical(Node p, Node q) { //same tree code
+        if (p == null && q == null) return true;           // both null → match
+        if (p == null || q == null) return false;          // one null → mismatch
+
+        if (p.data != q.data) return false;                // values don’t match
+        return isIdentical(p.left, q.left) && isIdentical(p.right, q.right); // recursively check left and right subtrees
     }
 
     public static void main(String[] args) {
-        Node p = new Node(1);
-        p.left = new Node(2);
-        p.right = new Node(3);
-        Node q = new Node(1);
-        q.left = new Node(2);
-        q.right = new Node(3);
-        System.out.println(check(p, q));
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+        Node subroot = new Node(2);
+        subroot.left = new Node(4);
+        subroot.right = new Node(5);
+        System.out.println(isSubtree(root, subroot));
     }
 }
---
+++ 20_Binary Tree/_14_rightLeftView.java
@@ -0,0 +1,70 @@
+import java.util.*;
+public class _14_rightLeftView {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    // levelorder traversal (RPA) approach same code, change 1 line
+    public static void rightView(Node root) {
+        if (root == null) return;
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        while (!q.isEmpty()) {
+            int count = q.size();
+            for (int i = 0; i < count; i++) {
+                Node curr = q.remove();
+                if(i == count-1) System.out.print(curr.data + " "); //change, add only last
+
+                if (curr.left != null) {
+                    q.add(curr.left);
+                }
+                if (curr.right != null) {
+                    q.add(curr.right);
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void leftView(Node root) {
+        if (root == null) return;
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        while (!q.isEmpty()) {
+            int count = q.size();
+            for (int i = 0; i < count; i++) {
+                Node curr = q.remove();
+                if(i == 0) System.out.print(curr.data + " "); //change, add only first
+
+                if (curr.left != null) {
+                    q.add(curr.left);
+                }
+                if (curr.right != null) {
+                    q.add(curr.right);
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+        rightView(root);
+        System.out.println("-------");
+        leftView(root);
+    }
+}
